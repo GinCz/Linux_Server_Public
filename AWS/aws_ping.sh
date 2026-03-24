644b65fa66a3fd43b255c0adf4bdae42857011d7
VladiMIR Bulantsev <gin@volny.cz>
2026-03-24 20:46:27 +0100

    Remove: AWS/aws_ping.sh v2026-03-24

--- AWS/aws_ping.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Description: Ping benchmark for AWS regions to check network latency.
-# Alias: awsping
-R="ec2.eu-central-1.amazonaws.com ec2.us-east-1.amazonaws.com ec2.eu-north-1.amazonaws.com ec2.ap-northeast-1.amazonaws.com"; for h in $R; do echo -n "Ping $h: "; ping -c 3 -W 2 $h 2>/dev/null | awk -F'/' 'END{ print (/^rtt/ ? $5" ms" : "TIMEOUT") }'; done

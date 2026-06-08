bec741612492f90457225560bb61357825636006
VladiMIR Bulantsev | GinCz <gin@volny.cz>
2026-06-08 22:15:21 +0200

    remove: obsolete vpn_hard_shield.sh (replaced by vpn_shield.sh)

--- VPN/vpn_hard_shield.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Description: Disk cleanup and basic IPTables blocklist for weak VPN nodes.
-# Alias: vpnshield
-apt-get update -qq && apt-get install -y fail2ban -qq; S=$(echo $SSH_CLIENT | awk '{print $1}'); [ -n "$S" ] && iptables -I INPUT -s "$S" -p tcp --dport 22 -j ACCEPT 2>/dev/null; B="144.76.32.114 185.177.72.56"; for i in $B; do iptables -I INPUT -s "$i" -j DROP 2>/dev/null; done; journalctl --vacuum-time=1d; rm -rf /var/log/*.gz /var/log/*.1 2>/dev/null; echo "VPN Hard Shield applied. Logs vacuumed."
